e2c3f7ebb72e0f44f407f34b6aaf95871a41d7a2
pederpus <pederpus@pm.me>
2022-01-09 17:48:03 +0100

    Change font

--- config.h
+++ config.h
@@ -10,8 +10,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SourceSansPro-Bold:size=10" };
-static const char dmenufont[]       = "SourceSansPro-Bold:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style:medium:size=10",
+                                        "Material Design Icons-Regular:size=10",
+                                      };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#282828"; /* #222222 background color */
 static const char col_gray2[]       = "#3c3836"; /* #444444 inactive window border color */
 static const char col_gray3[]       = "#bbbbbb"; /* #bbbbbb font color */
@@ -36,7 +38,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.75; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
